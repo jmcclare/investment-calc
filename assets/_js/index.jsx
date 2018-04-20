6a6a72ad6450c2652124b6b7eb1827e8fa00d411
Jonathan McClare <git.jmcclare@recursor.net>
2018-04-20 18:02:47 -0400

    add years field

--- assets/_js/index.jsx
+++ assets/_js/index.jsx
@@ -50,30 +50,54 @@ class Table extends React.Component {
 }
 
 
-class VarsForm extends React.Component {
+class NumField extends React.Component {
   constructor(props) {
     super(props)
 
-    this.handleYearlyContribChange = this.handleYearlyContribChange.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
 
-  handleYearlyContribChange(e) {
-    this.props.onYearlyContribChange(e.target.value);
+  handleChange(e) {
+    this.props.onChange(e.target.value);
+  }
+
+  render() {
+    return (
+      <p>
+        <label htmlFor={this.props.varName}>{this.props.label}:</label>
+        <span> </span>
+        <input
+          name={this.props.name}
+          value={this.props.value}
+          onChange={this.handleChange}
+        />
+      </p>
+    )
+  }
+}
+
+
+class VarsForm extends React.Component {
+  constructor(props) {
+    super(props)
   }
 
   render() {
     const handleYearlyContribChange = this.handleYearlyContribChange
     return (
       <form className="vars-form">
-        <p>
-          <label htmlFor="yearlycontrib">Yearly Contribution:</label>
-          <span> </span>
-          <input
-            name="yearlycontrib"
-            value={this.props.yearlyContrib}
-            onChange={handleYearlyContribChange}
-          />
-        </p>
+        <NumField
+          name="yearlyContrib"
+          label="Yearly Contribution"
+          value={this.props.yearlyContrib}
+          onChange={this.props.onYearlyContribChange}
+        />
+        <NumField
+          name="yearls"
+          label="Years"
+          value={this.props.years}
+          onChange={this.props.onYearsChange}
+        />
       </form>
     )
   }
@@ -85,6 +109,7 @@ class ICalc extends React.Component {
     super(props)
 
     this.handleYearlyContribChange = this.handleYearlyContribChange.bind(this)
+    this.handleYearsChange = this.handleYearsChange.bind(this)
 
     this.state = {
       startingTotal: 0.00,
@@ -108,6 +133,19 @@ class ICalc extends React.Component {
     })
   }
 
+  handleYearsChange(number) {
+    this.setState((prevState, props) => {
+      // Scrub the input
+      const fmtValue = Number(number).toFixed(0)
+      debug('in handleYearsChange, fmtValue: ' + fmtValue)
+      if (Number.isNaN(fmtValue)) {
+        return { years: prevState.years }
+      } else {
+        return { years: fmtValue }
+      }
+    })
+  }
+
   render() {
     var data = calcData(this.state)
     return (
@@ -115,6 +153,8 @@ class ICalc extends React.Component {
         <VarsForm
           yearlyContrib={this.state.yearlyContrib}
           onYearlyContribChange={this.handleYearlyContribChange}
+          years={this.state.years}
+          onYearsChange={this.handleYearsChange}
         />
         <Table
           data={data}
@@ -129,6 +169,7 @@ var calcData = function(params) {
   var data = []
   var total = params.startingTotal
   debug('in calcData, params.yearlyContrib: ' + params.yearlyContrib)
+  debug('in calcData, params.years: ' + params.years)
   for (var i = 0; i <= params.years; i++) {
     data.push([i, total])
     total = total * (1 + params.growthRate) + params.yearlyContrib
