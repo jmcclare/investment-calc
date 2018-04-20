0adf3b2d5825220fd5a627d468cfac7c59f86732
Jonathan McClare <git.jmcclare@recursor.net>
2018-04-20 12:13:57 -0400

    create basic static table

--- assets/_js/index.jsx
+++ assets/_js/index.jsx
@@ -2,31 +2,80 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
-class Toggle extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {isToggleOn: true};
+var Row = function(props) {
+  return (
+    <tr>
+      <td className="year">{props.item[0]}</td>
+      <td className="total">$ {(props.item[1]).toFixed(2)}</td>
+    </tr>
+  )
+}
 
-    // This binding is normally necessary to make `this` work in the callback,
-    // but not with the exprimental public class fields syntax enabled by
-    // Babel’s transform-class-properties plugin.
-    //this.handleClick = this.handleClick.bind(this);
-  }
 
-  handleClick = () => {
-    this.setState(prevState => ({
-      isToggleOn: !prevState.isToggleOn
-    }));
-  }
-
+class Table extends React.Component {
   render() {
+
+    const rows = this.props.data.map(item => {
+      return (
+        <Row key={item[0]} item={item} />
+      )
+    })
+
     return (
-      <button onClick={this.handleClick}>
-        {this.state.isToggleOn ? 'ON' : 'OFF'}
-      </button>
-    );
+      <div className="financial-results">
+        <table>
+          <thead>
+            <tr>
+              <th>Year</th>
+              <th>Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            {rows}
+          </tbody>
+        </table>
+      </div>
+    )
   }
 }
 
 
-export default Toggle
+class ICalc extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      startingTotal: 0.00,
+      growthRate: 0.07,
+      spendingRate: 0.035,
+      inflationRate: 0.03,
+      yearlyContribution: 30000.00,
+      years: 10
+    }
+  }
+
+  render() {
+    var data = calcData(this.state)
+    return (
+      <div className="icalc">
+        <Table
+          data={data}
+        />
+      </div>
+    )
+  }
+}
+
+
+var calcData = function(params) {
+  var data = []
+  var total = params.startingTotal
+  for (var i = 0; i <= params.years; i++) {
+    data.push([i, total])
+    total = total * (1 + params.growthRate) + params.yearlyContribution
+  }
+  return data
+}
+
+
+export default ICalc
